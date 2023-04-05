b15d6975ec8c577e3c0c0301123b607031ebd681
Louis Dyer <111328948+doilyuser@users.noreply.github.com>
2023-04-06 11:34:51 +1200

    Finished History and OneMeeting Component

--- client/components/History.tsx
+++ client/components/History.tsx
@@ -2,17 +2,49 @@ import OneMeeting from './OneMeeting'
 import { useAppSelector } from '../hooks'
 
 function History() {
+  const d1 = new Date().getTime()
+  const d2 = d1 - 100000000
+  const d3 = d1 - 200000000
+
   const fakeDataArr = [
-    { title: 'Meeting One', date: '17/04/93', id: 1 },
-    { title: 'Meeting Two', date: '18/04/93', id: 2 },
-    { title: 'Meeting Three', date: '19/04/93', id: 3 },
+    {
+      meeting_name: 'Meeting One',
+      duration: 10,
+      start_time: d1,
+      total_cost: 100000,
+      attendees: 10,
+    },
+    {
+      meeting_name: 'Meeting Two',
+      duration: 10,
+      start_time: d2,
+      total_cost: 120000,
+      attendees: 10,
+    },
+    {
+      meeting_name: 'Meeting Three',
+      duration: 10,
+      start_time: d3,
+      total_cost: 300000,
+      attendees: 10,
+    },
   ]
+  // const meetings = useAppSelector((state) => state.data)
 
   return (
     <div className="container">
       <h2 className="title is-2">Meeting history</h2>
-      {fakeDataArr.map((e, i) => {
-        return <OneMeeting key={i} title={e.title} date={e.date} tag={e.id} />
+      {fakeDataArr.map((e) => {
+        return (
+          <OneMeeting
+            key={e.start_time}
+            title={e.meeting_name}
+            duration={e.duration}
+            date={e.start_time}
+            cost={e.total_cost}
+            attendees={e.attendees}
+          />
+        )
       })}
     </div>
   )
--- client/components/OneMeeting.tsx
+++ client/components/OneMeeting.tsx
@@ -1,17 +1,27 @@
 import { Link } from 'react-router-dom'
+import { useAppDispatch } from '../hooks'
 
 function OneMeeting(props: any) {
-  const clickHandler = () => {
+  const dateString = new Date(props.date).toLocaleDateString()
+
+  // const dispatch = useAppDispatch()
+
+  const fakeClickHandler = () => {
     console.log('clicked delete!')
   }
+
+  // const deleteMeeting = (date: number) => {
+  //   dispatchEvent(deleteMeetingThunk(date))
+  // }
+  //swap over when backend ready
 
   return (
     <>
       <div className="container has-text-centered">
         <h3 className="title is-3">{props.title}</h3>
-        <p>{props.date}</p>
-        <Link to={'/history/' + props.tag}>More details...</Link>
-        <button onClick={clickHandler}>delete</button>
+        <p>Date: {dateString}</p>
+        <Link to={'/history/' + props.date}>More details...</Link>
+        <button onClick={() => fakeClickHandler()}>delete</button>
       </div>
     </>
   )
