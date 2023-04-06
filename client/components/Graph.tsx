71f9b5eac2e92ebe0f530287d64499d7483df260
Ryan-Kendrick <118949957+Ryan-Kendrick@users.noreply.github.com>
2023-04-07 10:23:06 +1200

    proof of concept

--- client/components/Graph.tsx
+++ client/components/Graph.tsx
@@ -7,7 +7,7 @@ function Graph() {
       id: 1,
       meeting_name: 'talking about isaac',
       duration: 3600000,
-      start_time: 1680570000000,
+      start_time: new Date(1680570000000),
       total_cost: 13234,
       attendees: 4,
     },
@@ -15,7 +15,7 @@ function Graph() {
       id: 2,
       meeting_name: 'talking about pat',
       duration: 3600000,
-      start_time: 1677981600000,
+      start_time: new Date(1677981600000),
       total_cost: 20145,
       attendees: 2,
     },
@@ -23,14 +23,51 @@ function Graph() {
       id: 3,
       meeting_name: 'fun time',
       duration: 3600000,
-      start_time: 1680292800000,
+      start_time: new Date(1680292800000),
       total_cost: 54132,
       attendees: 3,
     },
-    {
-      total_cost: 52134,
-    },
   ]
+
+  const totalByMonth = meetings.reduce(
+    (accumulator, oneMeeting) => {
+      const month = oneMeeting.start_time.getMonth()
+      accumulator[month] += oneMeeting.total_cost
+      return accumulator
+    },
+    {
+      1: 0,
+      2: 0,
+      3: 0,
+      4: 0,
+      5: 0,
+      6: 0,
+      7: 0,
+      8: 0,
+      9: 0,
+      10: 0,
+      11: 0,
+      12: 0,
+    } as Record<number, number>
+  )
+
+  console.log(totalByMonth)
+
+  // const monthified = meetings.map((el) => {
+  //   month = el.start_time.getMonth()
+  // })
+
+  // const yearArray = [[], [], [], [], [], [], [], [], [], [], [], []]
+
+  // meetings.map((el) => {
+  //   const monthIndex = el.start_time.getMonth()
+  //   yearArray[monthIndex - 1].push(el.total_cost)
+  // })
+
+  // 1. reassign start_time to the month i.e. 02 using .getMonth()
+  // 2. mapping over the meetings array, where the start_time property is 01, sum total_cost and push it to an empty array
+  // 3. do this 12 times for each month
+  // 4. we now have our data!
 
   const labels = [
     'January',
