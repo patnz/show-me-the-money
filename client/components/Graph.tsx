481a392252acf91489aeddde5bcfd6897b1ebe49
Ryan-Kendrick <118949957+Ryan-Kendrick@users.noreply.github.com>
2023-04-07 10:53:39 +1200

    ready for data from store

--- client/components/Graph.tsx
+++ client/components/Graph.tsx
@@ -1,8 +1,10 @@
 import { Line } from 'react-chartjs-2'
 import Chart from 'chart.js/auto'
+import { useAppSelector } from '../hooks'
 
 function Graph() {
   // REPLACE MEETING WITH REAL DATA
+
   const meetings = [
     {
       id: 1,
