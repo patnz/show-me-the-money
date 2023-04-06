a6dfc17a739480776a94b6cd88eaf6c55e770349
J-Haynes <114152650+J-Haynes@users.noreply.github.com>
2023-04-06 16:19:47 +1200

    add pseudocode for adding MeetingWithAttendeesInfo to meetings db

--- server/db/dbUtils.ts
+++ server/db/dbUtils.ts
@@ -74,3 +74,15 @@ export async function getMeetingsWithAttendees(): Promise<
   }
   return newMeetingsArr
 }
+
+// todo: write function that takes a MeetingWithAttendeesInfo object as a parameter
+
+//receive MeetingWithAttendeesInfo as data from front end
+//get current meeting id
+//for each attendee in the current meeting
+//-----//add attendee to attendees table
+//-----//add entry to link attendee and meeting in join table
+//remove attendees data array from meeting object
+//add meeting to meetings table
+
+//write function that gets attendee_id and meeting_id and adds to join table
